f7662c9a3a180a3dac2d948feddb6b7fd68cdec2
Aaqu <aaquteam@gmial.com>
2024-12-27 19:02:06 +0100

    pierwsza wersja polaczenia z baza

--- Aplikacja_Bankowa/HomePage.cs
+++ Aplikacja_Bankowa/HomePage.cs
@@ -7,16 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Aplikacja_Bankowa.Services;
 
 namespace Aplikacja_Bankowa
 {
     public partial class HomePage : Form
     {
-        public HomePage()
+        private readonly DatabaseConnection dbConnection;
+        public HomePage(DatabaseConnection dbConnection)
         {
             InitializeComponent();
 
             this.Text = "Strona Domowa";
+
+            // Przypisanie przekazanego połączenia do pola klasy
+            this.dbConnection = dbConnection;
+        }
+
+        private void HomePage_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- Aplikacja_Bankowa/Program.cs
+++ Aplikacja_Bankowa/Program.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Aplikacja_Bankowa.Services;
 
 namespace Aplikacja_Bankowa
 {
     internal static class Program
     {
+        // Jednorazowa inicjalizacja obiektu DatabaseConnection
+        public static DatabaseConnection DbConnection { get; private set; }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,7 +20,16 @@ namespace Aplikacja_Bankowa
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+
+            // Tworzenie instancji DatabaseConnection
+            DbConnection = new DatabaseConnection();
+
+            // Test połączenia przy starcie aplikacji (opcjonalnie)
+            DbConnection.TestConnection();
+
+            // Przekazanie połączenia do HomePage
+            Application.Run(new Login(DbConnection));
+
         }
     }
 }
